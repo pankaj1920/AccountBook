a57879f62463017e86c6db0f5a7ddb85c5d83fc4
BOHRA <bohrpankaj1920@gmail.com>
2021-03-12 19:14:47 +0530

    inital

---
+++ app/src/main/java/com/unitedsoftek/accountbook/ui/auth/fragment/CompanyRegistrationFragment.kt
@@ -0,0 +1,21 @@
+package com.unitedsoftek.accountbook.ui.auth.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.unitedsoftek.accountbook.R
+
+
+class CompanyRegistrationFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_company_registration, container, false)
+    }
+}
